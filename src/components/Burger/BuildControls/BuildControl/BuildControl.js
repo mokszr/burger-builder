c6c46caef827e7b725a996336687e05c976ebdc9
batur.seker <batur.seker@smartict.com.tr>
2019-01-28 15:46:55 +0300

    update

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import classes from './BuildControl.css';
+
+const buildControl = (props) => (
+    <div className={classes.BuildControl}>
+        <div className={classes.Label}>{props.label}</div>
+        <button
+            className={classes.Less}
+            onClick={props.added}
+            // disabled={props.disabled}
+        >Less
+        </button>
+        <button
+            className={classes.More}
+            onClick={props.removed}
+        >More
+        </button>
+    </div>
+);
+
+export default buildControl;
